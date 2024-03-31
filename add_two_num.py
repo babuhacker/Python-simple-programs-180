89697a02df51522f917df1ee404025ad68331e05
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-03-31 14:08:28 +0530

    Update add_two_num.py

--- add_two_num.py
+++ add_two_num.py
@@ -1,4 +1,13 @@
-num1 = 30
-num2 = 40
+#num1 = 30
+#num2 = 40
+#sum = num1+num2
+#print("The sum of given numbers is ", sum)
+
+
+num1 = float(input("Enter a number:"))
+num2 = float(input("Enter another number:"))
+
 sum = num1+num2
-print("The sum of given numbers is ", sum)
+print("The sum of the provided two numbers is ", sum)
+
+
