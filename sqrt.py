981be6e0158e69c17935e4af1ed9b91e1c36bc9d
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-04-01 22:14:51 +0530

    Add files via upload

---
+++ sqrt.py
@@ -0,0 +1,17 @@
+
+# to find sqrt of the given number
+
+# using exponentiation
+
+#num = int(input("enter a number here: "))
+#sr = num**(1/2)
+#print("the square root of the given number is ", sr)
+
+# using math module
+
+import math
+
+num = int(input("enter a number here: "))
+sr = math.sqrt(num)
+print("the square root of the given number is ", sr)
+
