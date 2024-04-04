d445fde748d74feefb716d2b87cc610b19d5e4b6
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-04-04 21:14:33 +0530

    Add files via upload

---
+++ swap_two_variables.py
@@ -0,0 +1,28 @@
+
+# Program to swap two variabbles
+
+# using three variable
+
+'''
+
+x = 13
+y = 12
+temp = x
+print("the value of temp variable is: ", temp)
+
+x = y
+print("the value of x  is: ", x)
+
+y = temp
+print("the value of y  is: ", y)
+
+'''
+
+# using two variable
+x = 12
+y = 13
+
+x,y = y,x
+
+print("the value of x  is: ", x)
+print("the value of y  is: ", y)
