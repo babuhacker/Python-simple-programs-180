e51deb223946359f5eb1dcb6d3da86efcb721eed
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-04-08 01:23:47 +0530

    Add files via upload

---
+++ number_odd_or_even.py
@@ -0,0 +1,8 @@
+# program to find a Number Odd or Even
+
+num = int(input("enter a number here:"))
+
+if num % 2 == 0:
+    print("It is an number is even")
+else:
+    print("It is an number is odd")
---
+++ number_positive_negative_or_zero.py
@@ -0,0 +1,10 @@
+# program to check if a number is Positive Negative or Zero
+
+num = int(input("enter a number here:"))
+
+if num > 0:
+    print("It is a positive number")
+elif num == 0:
+    print("It is Zero")
+else:
+    print("It is a negative number")
