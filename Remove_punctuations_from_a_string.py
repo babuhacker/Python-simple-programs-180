a59ed6316f037a99b6dbf584e19aa23797d25f87
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-04-30 22:13:53 +0530

    Add files via upload

---
+++ Remove_punctuations_from_a_string.py
@@ -0,0 +1,14 @@
+# Program to Remove Punctuations From a String
+# () , {} , -  ,_ ,",' ,= ,! ,<>
+
+punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+string = input("enter anything here: ")
+
+empty_str = ""
+
+for i in string:
+    if i not in punc:
+        empty_str += i
+
+print(empty_str)
