19aaa9bfc7662e3359a4b23cf464149e1efc49ee
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-05-02 22:56:08 +0530

    Add files via upload

---
+++ Illustrate_different_set_operations.py
@@ -0,0 +1,12 @@
+# Program to Illustrate Different Set Operations
+
+a = {1, 2, 3, 6, 8, 9}
+b = {3, 4, 5, 6, 7, 8}
+
+print("The union is", a | b)
+
+print("THe Intersection is", a & b)
+
+print("The Difference is", a - b)
+
+print("The Symmetric difference", a ^ b)
