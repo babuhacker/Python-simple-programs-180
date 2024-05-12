be404c96ce9bfa6eb8990425bb263d85b818ad92
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-05-13 00:43:03 +0530

    Add files via upload

---
+++ Check if a List is Empty.py
@@ -0,0 +1,21 @@
+# Program  to Check if a List is Empty
+
+# using boolean operation
+
+my_list = []
+if not my_list:
+    print("the list is empty")
+
+
+# using len() function
+
+my_list = []
+if len(my_list) == 0:
+    print("the list is empty")
+
+
+# using []
+
+my_list = [12, 4, 56, 7]
+if my_list== []:
+    print("The list is empty")
