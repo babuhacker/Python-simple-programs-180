c89448f0dc69ecbf3d46683fae583b39ef2ac0ad
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-05-15 23:46:19 +0530

    Add files via upload

---
+++ Check if a Key is Already Present in a Dictionary.py
@@ -0,0 +1,9 @@
+# Program to Check if a Key is Already Present in a Dictionary
+
+friends = {"Rachel": "Ross", "Monica": "Chandler", "Phoebe": "Joe"}
+print("lists are", friends)
+name = input("enter aa key here: ")
+
+if name in friends.keys():
+    print("it is present")
+
