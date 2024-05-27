7d7ce0c25341711cb6e230e9aa3f80aa2cb30bb3
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-05-27 23:20:59 +0530

    Add files via upload

---
+++ Capitalize the First Character of a String.py
@@ -0,0 +1,15 @@
+# Program to Capitalize the First Character of a String
+
+# Example 1: Using list slicing
+
+my_string = "programiz is Lit"
+
+print(my_string[0].upper() + my_string[1:])
+
+# Example 2: Using inbuilt method capitalize()
+
+my_string = "programiz is Lit"
+
+cap_string = my_string.capitalize()
+
+print(cap_string)
