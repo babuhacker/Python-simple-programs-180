88cca42441e292d5c14d238e1e5392672f6173ce
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-06-06 15:46:17 +0530

    Add files via upload

---
+++ Compute the Power of a Number.py
@@ -0,0 +1,13 @@
+# Program to Compute the Power of a Number
+
+
+base = 3
+exponent = 4
+
+result = 1
+
+while exponent != 0:
+    result *= base
+    exponent -= 1
+
+print("Answer = " + str(result))
