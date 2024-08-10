bae8461eda892d18e694464eff831c16648cabdd
babuhacker <86510753+babuhacker@users.noreply.github.com>
2024-08-10 20:17:43 +0530

    Add files via upload

---
+++ DAY62-10-08-24/Program1.py
@@ -0,0 +1,25 @@
+# Majority Element
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Solution 1
+        res, count = 0, 0
+
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
+        return res
+
+       # Solution 2
+
+"""
+        count = {}
+        res, maxCount = 0, 0
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+            res = n if count[n] > maxCount else res
+            maxCount = max(count[n], maxCount)
+        return res
+"""
